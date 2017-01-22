a524ed51f171a96900621877b92f21c06b1b2263
pkatira7 <pkatira@vmware.com>
2017-01-23 02:32:01 +0530

    Add files via upload

---
+++ SubClass.java
@@ -0,0 +1,17 @@
+
+public class SubClass extends SuperClass {
+
+	public SubClass() {
+		super();
+		System.out.println("SubClass's Constructor");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              
+		SubClass sub = new SubClass();
+		
+	}
+
+}
---
+++ SuperClass.java
@@ -0,0 +1,10 @@
+
+public class SuperClass {
+ 
+ 
+
+	public SuperClass() {
+	//super();
+	System.out.println("SuperClass's Constructor");
+}
+}
